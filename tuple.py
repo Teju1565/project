c9f24bffd542bd917a7b6202622878bacee058e5
Teju1565 <86614471+Teju1565@users.noreply.github.com>
2021-07-13 22:28:37 -0700

    create tuple.py
    This file is shows the ouptut of tuple on the screen

---
+++ tuple.py
@@ -0,0 +1,12 @@
+#Tuple Methods
+
+tuple=["1","2","3","1"]
+
+#count()=Returns the number of times a specified value occurs in a tuple
+count=tuple.count("1")
+print("tuple count=",count)
+
+
+#index()=searches the tuple for a specified value and returns the position of where it was found
+index =tuple.index("2")
+print("Tuple Index=",index)
